09bf2dc6baca42e0b942ab54321538c7911e79de
Alesandro Silva <silva_alesandro@outlook.com>
2021-03-16 05:58:17 -0400

    feat: creat user struct

---
+++ lib/user.ex
@@ -0,0 +1,21 @@
+defmodule FlightBooking.Users.User do
+  @keys [:id, :name, :cpf, :email]
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(id, name, cpf, email) 
+    when is_bitstring(cpf) do
+    {:ok,
+      %__MODULE__{
+        id: id
+        name: name,
+        cpf: cpf,
+        email: email,
+      }
+    }    
+  end
+
+  def build(_id, _name ,_cpf ,_email), 
+    do: {:error, "Invalid parameters"} 
+end
