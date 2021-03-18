a718ff43fed849801e1f4963eda35d7a137b7f4f
Alesandro Silva <silva_alesandro@outlook.com>
2021-03-17 23:22:30 -0400

    feat: create create_or_update module

---
+++ lib/users/create_or_update.ex
@@ -0,0 +1,16 @@
+defmodule FlightBooking.Users.CreateOrUpdate do
+  alias FlightBooking.Users.User
+  alias FlightBooking.Users.Agent, as: UserAgent
+  def call(%{name: name, cpf: cpf, email: email}) do
+    name
+    |> User.build( cpf, email)
+    |> save_user()
+  end
+
+  defp save_user({:ok, %User{id: id} = user}) do
+    case UserAgent.save(user) do
+      :ok -> {:ok, id}
+    end
+  end
+  defp save_user({:error, _reason = error}), do: error
+end
