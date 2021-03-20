5c0d27c5c6f0636557b2157900334c3761030e9e
Alesandro Silva <silva_alesandro@outlook.com>
2021-03-20 17:44:59 -0400

    feat: create flight_booking facade

--- lib/flight_booking.ex
+++ lib/flight_booking.ex
@@ -1,8 +1,21 @@
 defmodule FlightBooking do
   alias FlightBooking.Users.Agent, as: UserAgent
-  alias FlightBooking.Users.CreateOrUpdate
+  alias FlightBooking.Users.CreateOrUpdate, as: CreateUpdateUser
+  alias FlightBooking.Bookings.Agent, as: BookingAgent
+  alias FlightBooking.Bookings.CreateOrUpdate, as: CreateUpdateBooking 
 
-  def start_agents, do: UserAgent.start_link(%{})
+  def init_user_agent do
+    UserAgent.start_link(%{})
+  end
 
-  defdelegate create_user(params), to: CreateOrUpdate, as: :call
+  def init_booking_agent do
+    BookingAgent.start_link(%{})                             end
+
+  def get_user(id), do: UserAgent.get(id)
+
+  def get_booking(id), do: BookingAgent.get(id)
+
+  defdelegate create_user(params), to: CreateUpdateUser, as: :call
+
+  defdelegate create_booking(params), to: CreateUpdateBooking, as: :call
 end
