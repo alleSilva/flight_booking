7724ff80abdc15fcc0e51ae4d740f9a88252ac6a
Alesandro Silva <silva_alesandro@outlook.com>
2021-03-20 17:39:17 -0400

    feat: create struct booking

--- lib/bookings/booking.ex
+++ lib/bookings/booking.ex
@@ -1,22 +1,27 @@
 defmodule FlightBooking.Bookings.Booking do
+  alias FlightBooking.Users.User
   @keys [:id, :complete_date, :from_city, :to_city, :user_id]
   @enforce_keys @keys
 
   defstruct @keys
 
-  def create_booking(id, complete_date, from_city, to_city, user_id)
+  def build(complete_date, from_city, to_city, user)
     when is_bitstring(from_city) and is_bitstring(to_city) do
-    {:ok,
-      %__MODULE__{
-        id: id,
-        complete_date: complete_date,
+    complete_date
+    |> NaiveDateTime.from_iso8601() 
+    |> build_booking(from_city, to_city, user)
+  end
+
+  def build(_date, _from_city, _to_city, _user), do: {:error, "Invalid parameters"}
+
+  defp build_booking({:ok, date}, from_city, to_city, %User{id: user_id}) do
+    {:ok,                                                        %__MODULE__{
+        id: UUID.uuid4(),                                          complete_date: date,
         from_city: from_city,
         to_city: to_city,
-        user_id: user_id
-      }
+        user_id: user_id                                         }
     }
   end
 
-  def reate_booking(_id, _complete_date, _from_city, _to_city, _user_id),
-    do: {:error, "Invalid parameters"}
+  defp build_booking({:error, _reason}, _from_city, _to_city, _user), do: {:error, "date must be string in format yyyy-MM-dd hh:mm:ss"}
 end
